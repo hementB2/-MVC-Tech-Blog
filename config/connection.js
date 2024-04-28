d193d34b81d28d560c71ecdf0da1650109f9c0b2
hementB2 <153564981+hementB2@users.noreply.github.com>
2024-04-28 15:43:10 -0400

    Recomment add

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,14 @@
 const Sequelize = require('sequelize');
 require('dotenv').config(); // Import sensitive data from .env
 
-const sequelize = process.env.JAWSDB_URL 
-  ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
-      host: 'localhost',
-      dialect: 'mysql',
+// Create a Sequelize instance based on the environment
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL) // Use JAWSDB_URL for Heroku deployment
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost', // Local database host
+      dialect: 'mysql', // Specify MySQL as the database dialect
       dialectOptions: {
-        decimalNumbers: true,
+        decimalNumbers: true, // Ensure decimal numbers are handled properly
       },
     });
 
