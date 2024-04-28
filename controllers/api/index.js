e75ce5697ddbc443bba340a91204d13e74249b64
hementB2 <153564981+hementB2@users.noreply.github.com>
2024-04-28 18:33:59 -0400

    Coorection in user.routes & Index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,13 @@
 // Import the required modules
-const router = require("express").Router(); // Import the Router module from Express
-const userRoutes = require("./user-routes"); // Import user-related routes
-const postRoutes = require("./post-routes"); // Import post-related routes
-const commentRoutes = require("./comment-routes"); // Import comment-related routes
+const router = require("express").Router();
+const userRoutes = require("./user-routes");
+const postRoutes = require("./post-routes");
+const commentRoutes = require("./comment-routes");
 
 // Set up the routes
-router.use("/users", userRoutes); // Use user-related routes
-router.use("/posts", postRoutes); // Use post-related routes
-router.use("/comments", commentRoutes); // Use comment-related routes
+router.use("/users", userRoutes); // Routes for user-related functionality
+router.use("/posts", postRoutes); // Routes for post-related functionality
+router.use("/comments", commentRoutes); // Routes for comment-related functionality
 
 // Export the router
 module.exports = router;
