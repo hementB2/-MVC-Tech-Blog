fabde757b9cc27048bb5515dc6526498348a2951
hementB2 <153564981+hementB2@users.noreply.github.com>
2024-04-28 17:23:14 -0400

    Model MVC

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -1,43 +1,45 @@
 const { Comment } = require("../models");
 
+// Data for seeding comments
 const commentData = [
   {
-    comment_text: "Great article!",
+    comment_text: "Excellent write-up!",
     user_id: 1,
     post_id: 1,
   },
   {
-    comment_text: "I agree with you!",
+    comment_text: "I share the same perspective!",
     user_id: 2,
     post_id: 1,
   },
   {
-    comment_text: "I disagree with you!",
+    comment_text: "I respectfully disagree!",
     user_id: 3,
     post_id: 1,
   },
   {
-    comment_text: "I agree with you!",
+    comment_text: "I concur with your points!",
     user_id: 4,
     post_id: 1,
   },
   {
-    comment_text: "I disagree with you!",
+    comment_text: "I have a different viewpoint!",
     user_id: 5,
     post_id: 1,
   },
   {
-    comment_text: "Great article!",
+    comment_text: "Well articulated!",
     user_id: 1,
     post_id: 2,
   },
   {
-    comment_text: "I agree with you!",
+    comment_text: "You've made some valid points!",
     user_id: 2,
     post_id: 2,
   }
 ];
 
+// Function to seed comments
 const seedComments = () => Comment.bulkCreate(commentData);
 
 module.exports = seedComments;
