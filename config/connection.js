784dbd37c246747d50e7d18121971e75bcefd1cb
hementB2 <153564981+hementB2@users.noreply.github.com>
2024-04-29 21:44:15 -0400

    Heroku

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost', // Local database host
       dialect: 'mysql', // Specify MySQL as the database dialect
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true, // Ensure decimal numbers are handled properly
       },
